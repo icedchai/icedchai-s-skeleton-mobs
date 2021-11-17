83f79180d18e6f7ce956918bcc686047ce2d2a7c
icedchai <70030002+icedchai@users.noreply.github.com>
2021-11-16 21:46:02 -0600

    changed spawn chances

--- src/main/java/me/icedchai/skeletonmobs/common/mixin/spawnMixin.java
+++ src/main/java/me/icedchai/skeletonmobs/common/mixin/spawnMixin.java
@@ -1,6 +1,5 @@
 package me.icedchai.skeletonmobs.common.mixin;
 
-import me.icedchai.skeletonmobs.IcedchaisSkeletons;
 import me.icedchai.skeletonmobs.common.init.entity.EntityInit;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.world.biome.SpawnSettings;
@@ -14,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class spawnMixin {
     @Inject(method = "addMonsters", at = @At("HEAD"), cancellable = true)
     private static void spawnSKnight(SpawnSettings.Builder builder,  int zombieWeight, int zombieVillagerWeight, int skeletonWeight, CallbackInfo cir) {
-        builder.spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityInit.SKNIGHT, 100, 3, 5));
+        builder.spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityInit.SKNIGHT, 70, 4, 4));
     }
 
     @Inject(method="addSnowyMobs", at = @At("HEAD"), cancellable = true)
     private static void spawnStKnight(SpawnSettings.Builder builder, CallbackInfo cir){
-        builder.spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityInit.STKNIGHT, 80, 3, 5));
+        builder.spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityInit.STKNIGHT, 20, 4, 4));
     }
 }
